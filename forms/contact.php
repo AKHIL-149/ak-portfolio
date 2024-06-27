b55049964688a8e8792a416c38fdb5bda6fba7ae
Venkata Akhil Mettu <151084422+AKHIL-149@users.noreply.github.com>
2024-06-27 15:27:47 -0400

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -5,40 +5,37 @@
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
-  // email address of choice
-  $receiving_email_address = 'makhil@saintpeters.edu';
-  if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-    include($php_email_form);
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'contact@example.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
   } else {
-    die('Unable to load the "PHP Email Form" Library!');
+    die( 'Unable to load the "PHP Email Form" Library!');
   }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-
+  
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Add additional fields if needed
-  $contact->add_message($_POST['name'], 'From');
-  $contact->add_message($_POST['email'], 'Email');
-  $contact->add_message($_POST['message'], 'Message', 10);
-
-  // Add your SMTP credentials if you want to use SMTP to send emails
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'your_email@gmail.com',
-    'password' => 'your_password',
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
     'port' => '587'
   );
+  */
 
-  // Add any additional information you want to include in the email
-  $contact->add_attachment($_FILES['attachment'], 'Attachment');
-  $contact->add_field('Phone', '+1 (802) 261-5876');
-  $contact->add_field('Location', 'New York, USA');
-  $contact->add_field('LinkedIn', 'https://www.linkedin.com/in/venkata-akhil-mettu-51a48b277/');
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
